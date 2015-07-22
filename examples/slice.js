ab6a3df355b351d472a0714b80e914426e10c54f
Olivier Lalonde <olalonde@gmail.com>
2015-07-22 16:22:21 +0800

    Use array indexes instead of .get/.set

--- examples/slice.js
+++ examples/slice.js
@@ -1,4 +1,4 @@
-var Buffers = require('buffers');
+var Buffers = require('../');
 var bufs = Buffers();
 bufs.push(new Buffer([1,2,3]));
 bufs.push(new Buffer([4,5,6,7]));
@@ -7,3 +7,7 @@ bufs.push(new Buffer([8,9,10]));
 console.dir(bufs.slice(2,8))
 
 // Output: <Buffer 03 04 05 06 07 08>
+
+console.dir(bufs.get(2))
+
+// Output: 3
